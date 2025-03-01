6ef6bac440ee9ba042416f33a06052bfd606f2b8
axeld27 <axeldomguia@gmail.com>
2025-03-01 03:17:21 +0000

    finish

--- Lab5.py
+++ Lab5.py
@@ -5,28 +5,17 @@
 # *   *
 # *****
 def hollow_square(n):
-    result = ""
-    for i in range (0,n):
-        result += "*"
-    result += "\n"
     
-    if n >= 5:
-        for i in range (0,n - 4):
-            result += "*   *"
-        result += "\n"
+    result = ""
+    for i in range (n):
+        for j in range (n):
+            if i == 0 or i== n-1 or j==0 or j == n-1:
+                result += "*"
+            else: 
+                result += " "
 
-        for i in range (0,n - 4):
-            result += "*   *"
-        result += "\n"
 
-        for i in range (0,n - 4):
-            result += "*   *"
         result += "\n"
-
-    for i in range (0,n):
-            result += "*"
-        result += "\n"
-
     return (result.strip())
 
 
@@ -56,4 +45,11 @@ def sum_of_natural_numbers(n):
 #  *****
 # *******
 def centered_star_pyramid(n):
-    return ""
+    result = ''
+    for i in range(n):
+        result += " " * (n-i-1)
+
+        result += "*" * (2*i+1)
+        
+        result += "\n"
+    return result.rstrip()
